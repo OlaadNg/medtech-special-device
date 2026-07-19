ccd82c7f5a835adf42c36510820cc381a724d04b
olabode itiola <itiolaolabode5@gmail.com>
2026-07-19 06:36:50 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,10 @@ import {
   Thermometer, BarChart3, Building2, Syringe
 } from 'lucide-react';
 import HeroSection from '../components/home/HeroSection';
-import TrustedBy from '../components/home/TrustedBy';
 import CareDivisions from '../components/home/CareDivisions';
 import StatsSection from '../components/home/StatsSection';
 import WhyChooseUs from '../components/home/WhyChooseUs';
 import FeaturedProducts from '../components/home/FeaturedProducts';
-import Testimonials from '../components/home/Testimonials';
 import NewsPreview from '../components/home/NewsPreview';
 import HomeCTA from '../components/home/HomeCTA';
 
@@ -21,12 +19,10 @@ export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <HeroSection />
-      <TrustedBy />
       <CareDivisions />
       <FeaturedProducts />
       <StatsSection />
       <WhyChooseUs />
-      <Testimonials />
       <NewsPreview />
       <HomeCTA />
     </div>
